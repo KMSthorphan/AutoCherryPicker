1769e143f740e92bace3158dc07ed67ab388906b
thanhpphan <thanhpphan@silverlakeaxis.com>
2023-11-21 20:03:27 +0700

    Update script

--- script.sh
+++ script.sh
@@ -7,7 +7,6 @@ read -p "Commit ID: " value3
 
 if [ -z "$value1" ] || [ -z "$value2" ] || [ -z "$value3" ]; then
   echo "Missing input"
-  read
   exit 1
 fi
 
@@ -18,11 +17,19 @@ branch_names=("release1" "release2")
 for branch_name in "${branch_names[@]}"; do
   # Generate tên cho branch
   echo -n "-------------------------------------------"
+  
   new_branch="${value1}/${branch_name}/${value2}"
-  echo -n "Creating branch $new_branch"
 
-  # Checkout từ branch đã được khai báo
-  git checkout $branch_name
+  echo -n "Creating branch $new_branch"
+  echo -n ""
+
+  # Kiểm tra xem branch đã tồn tại hay chưa
+  if git show-ref --quiet "refs/heads/$new_branch"; then
+    git checkout $new_branch
+  else
+    # Tạo mới branch nếu nó chưa tồn tại
+    git checkout -b $new_branch $branch_name
+  fi
 
   # Cherry pick từ value3
   git cherry-pick $value3
